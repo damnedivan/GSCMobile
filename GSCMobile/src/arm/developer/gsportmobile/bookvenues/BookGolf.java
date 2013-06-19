cd21fd1fdf5679a56ad65fa98cc2273b3b18bd74
damnedivan <tivanoedwin@gmail.com>
2013-06-19 10:55:31 +0700

    Added date picker

--- GSCMobile/src/arm/developer/gsportmobile/bookvenues/BookGolf.java
+++ GSCMobile/src/arm/developer/gsportmobile/bookvenues/BookGolf.java
@@ -1,30 +1,29 @@
 package arm.developer.gsportmobile.bookvenues;
 
-import android.content.BroadcastReceiver;
-import android.content.IntentFilter;
+import java.util.Calendar;
+
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.util.Log;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import arm.developer.gsportmobile.R;
-import arm.developer.gsportmobile.receiver.DeliveryReceiver;
-import arm.developer.gsportmobile.receiver.SentReceiver;
-import arm.developer.gsportmobile.utility.BookHelper;
 
 import com.actionbarsherlock.app.SherlockActivity;
 
 public class BookGolf extends SherlockActivity {
-	private static final String TAG = BookGolf.class.getSimpleName();
-	public static final String SMS_SENT = "SMS_SENT";
-	public static final String SMS_DELIVERED = "SMS_DELIVERED";
-	private final BroadcastReceiver sentReceiver = new SentReceiver();
-	private final BroadcastReceiver deliveryReceiver = new DeliveryReceiver();
 
-	Button btnGolf;
-	EditText namaGolf, alamatGolf, telpGolf;
+	private Button btnGolf, btnTglGolf;
+	private EditText namaGolf, alamatGolf, telpGolf;
+	private TextView tanggalGolf;
+	private int mYear;
+	private int mMonth;
+	private int mDay;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -32,28 +31,35 @@ public class BookGolf extends SherlockActivity {
 		setContentView(R.layout.book_golf_layout);
 
 		btnGolf = (Button) findViewById(R.id.btn_golfBook);
+		btnTglGolf = (Button) findViewById(R.id.btn_golfTanggal);
 		namaGolf = (EditText) findViewById(R.id.text_golfNama);
 		alamatGolf = (EditText) findViewById(R.id.text_golfAlamat);
 		telpGolf = (EditText) findViewById(R.id.text_golfTelp);
-
-		String namaG = namaGolf.getText().toString();
-		String alamatG = alamatGolf.getText().toString();
-		String telpG = telpGolf.getText().toString();
-
-		final String smsMessage = String.format("%s\n%s\n%s", namaG, alamatG,
-				telpG);
+		tanggalGolf = (TextView) findViewById(R.id.tv_golfTanggal);
 
 		btnGolf.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 
+				String namaG = namaGolf.getText().toString();
+				String alamatG = alamatGolf.getText().toString();
+				String telpG = telpGolf.getText().toString();
+				String tanggalG = tanggalGolf.getText().toString();
+
+				final String smsMessage = String.format("%s\n%s\n%s\n%s",
+						namaG, alamatG, telpG, tanggalG);
+
 				try {
-					BookHelper.sendSMS(getApplicationContext(),
-							"+6287792021743", smsMessage);
+
+					SmsManager smsM = SmsManager.getDefault();
+					smsM.sendTextMessage("+6287792021743", null, smsMessage,
+							null, null);
+					Toast.makeText(getApplicationContext(), "SMS Sent!",
+							Toast.LENGTH_LONG).show();
 				} catch (Exception e) {
 					Toast.makeText(getApplicationContext(),
-							"SMS faild, please try again later!",
+							"SMS failed, please try again later!",
 							Toast.LENGTH_LONG).show();
 					e.printStackTrace();
 				}
@@ -61,25 +67,41 @@ public class BookGolf extends SherlockActivity {
 			}
 		});
 
+		btnTglGolf.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				new DatePickerDialog(BookGolf.this, mDateSetListener, mYear,
+						mMonth, mDay).show();
+
+			}
+		});
+
+		final Calendar c = Calendar.getInstance();
+		mYear = c.get(Calendar.YEAR);
+		mMonth = c.get(Calendar.MONTH);
+		mDay = c.get(Calendar.DAY_OF_MONTH);
+		updateDisplay();
+
 	}
 
-	@Override
-	protected void onPause() {
-		try {
-			unregisterReceiver(sentReceiver);
-			unregisterReceiver(deliveryReceiver);
-		} catch (RuntimeException e) {
-			Log.e(TAG, "Receiver not Registered");
+	// TODO===========================DATEPICKER================================
+
+	private void updateDisplay() {
+		tanggalGolf.setText(new StringBuilder()
+				// Month is 0 based so add 1
+				.append(mMonth + 1).append("-").append(mDay).append("-")
+				.append(mYear).append(" "));
+	}
+
+	private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+		public void onDateSet(DatePicker view, int year, int monthOfYear,
+				int dayOfMonth) {
+			mYear = year;
+			mMonth = monthOfYear;
+			mDay = dayOfMonth;
+			updateDisplay();
 		}
-		super.onPause();
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-		registerReceiver(sentReceiver, new IntentFilter(SMS_SENT));
-		registerReceiver(sentReceiver, new IntentFilter(SMS_DELIVERED));
-		Log.d(TAG, "onResume");
-	}
+	};
 
 }
---
+++ GSCMobile/src/arm/developer/gsportmobile/utility/DialogHelper.java
@@ -0,0 +1,5 @@
+package arm.developer.gsportmobile.utility;
+
+public class DialogHelper {
+
+}
